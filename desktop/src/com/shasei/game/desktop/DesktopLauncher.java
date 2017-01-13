7c840c8f5c612cc29ecc42c634a18476576e0d0a
Alanian <bvdkki@googlemail.com>
2017-01-13 22:18:14 +0100

    first commit

---
+++ desktop/src/com/shasei/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shasei.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shasei.game.Shasei;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Shasei(), config);
+	}
+}
